d37659be8b3b2ed89647a16779b7167121339513
Chunhung (Brian) Liu <hong86323@gmail.com>
2022-06-19 16:09:25 +0800

    Replace with PWA sample (#2)
    * Replace with PWA sample
    
    * Fix command and style
    
    * Update github actions

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    // For components/Image.js
-    loader: "custom",
-  },
-};
+const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
