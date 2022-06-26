bbaa21ac425b49cba71c38030c88de2ada475ecf
Chunhung (Brian) Liu <hong86323@gmail.com>
2022-06-27 01:25:40 +0800

    Update homepage to the latest design (#6)
    * Update homepage
    
    * Fix shopping item tile vertically scrollable.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,105 @@
 import styles from "../styles/Home.module.css";
 
+const Ads = () => {
+  return <div className={styles.ads}>Ads reel</div>;
+};
+
+type MemberInfoProps = {
+  name: string;
+  credits: number;
+  ntdEquivalent: number;
+};
+
+const MemberInfo = ({ name, credits, ntdEquivalent }: MemberInfoProps) => {
+  const FirstRow = () => {
+    return (
+      <div className={styles.MemberInfoFirstColumn}>
+        <div>{name}</div>
+        <div>{credits} 點</div>
+      </div>
+    );
+  };
+  const SecondRow = () => {
+    return (
+      <div className={styles.MemberInfoSecondColumn}>
+        <div>捷客點</div>
+        <div>可退換 NT ${ntdEquivalent}</div>
+      </div>
+    );
+  };
+  return (
+    <div className={styles.MemberInfo}>
+      <FirstRow />
+      <SecondRow />
+    </div>
+  );
+};
+
+type ShortcutItemProps = {
+  text: string;
+};
+
+const ShortcutItem = ({ text }: ShortcutItemProps) => {
+  return (
+    <div className={styles.ShortcutItem}>
+      <div className={styles.ShortcutIcon}></div>
+      {text}
+    </div>
+  );
+};
+
+const Shortcuts = () => {
+  return (
+    <div className={styles.ShortcutSection}>
+      <ShortcutItem text={"我的票卡"} />
+      <ShortcutItem text={"到站時刻"} />
+      <ShortcutItem text={"捷運路網"} />
+      <ShortcutItem text={"我的優惠券"} />
+      <ShortcutItem text={"成就徽章"} />
+    </div>
+  );
+};
+
+const OnlineMall = () => {
+  return (
+    <div className={styles.OnlineMallSection}>
+      <div className={styles.OnlineMallTitle}>
+        <p>線上商城 ｜熱銷商品</p>
+        <p>查看更多</p>
+      </div>
+      <div className={styles.OnlineMallTilesSection}>
+        <div className={styles.OnlineMallTile} />
+        <div className={styles.OnlineMallTile} />
+        <div className={styles.OnlineMallTile} />
+      </div>
+    </div>
+  );
+};
+
+const Spaces = () => {
+  return (
+    <div className={styles.OnlineMallSection}>
+      <div className={styles.OnlineMallTitle}>
+        <p>捷空間</p>
+        <p>查看更多</p>
+      </div>
+      <div className={styles.OnlineMallTilesSection}>
+        <div className={styles.OnlineMallTile} />
+        <div className={styles.OnlineMallTile} />
+        <div className={styles.OnlineMallTile} />
+      </div>
+    </div>
+  );
+};
+
 export default function Home() {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Ads />
+      <MemberInfo name={"陳小名"} credits={328} ntdEquivalent={45} />
+      <Shortcuts />
+      <OnlineMall />
+      <Spaces />
     </div>
   );
 }
