e4f21497e621e7324990f77e19facdde8641b87c
brianliu <hong86323@gmail.com>
2022-06-30 01:46:59 +0800

    Add testing share button

--- pages/membership.tsx
+++ pages/membership.tsx
@@ -1,7 +1,21 @@
 const Membership = () => {
+  const shareButtonClickHandler = async () => {
+    const data = {
+      title: "web.dev",
+      text: "Check out web.dev.",
+      url: "https://web.dev/",
+    };
+    try {
+      await navigator.share(data);
+    } catch (err) {
+      alert(err);
+    }
+  };
+
   return (
     <>
       <h1>會員中心</h1>
+      <button onClick={shareButtonClickHandler}>Share</button>
     </>
   );
 };
