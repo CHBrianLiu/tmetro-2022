94cd5f79cdcfff71afd3264c746d6dc62f93aea0
Chunhung (Brian) Liu <hong86323@gmail.com>
2022-06-30 16:17:30 +0800

    Testing share button (#7)
    * Add testing share button
    
    * Add ShareButton component

---
+++ components/ShareButton.tsx
@@ -0,0 +1,30 @@
+import styles from "../styles/ShareButton.module.css";
+import ShareIcon from "../assets/share-icon.svg";
+import Image from "next/image";
+
+interface ShareButtonProps {
+  data: {
+    title: string;
+    text?: string;
+    url?: string;
+    files?: File[];
+  };
+}
+
+const ShareButton = ({ data }: ShareButtonProps) => {
+  const shareButtonClickHandler = async () => {
+    try {
+      await navigator.share(data);
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+  return (
+    <div onClick={shareButtonClickHandler} className={styles.Button}>
+      <Image src={ShareIcon} alt={"share icon"} />
+      Share
+    </div>
+  );
+};
+
+export default ShareButton;
--- pages/membership.tsx
+++ pages/membership.tsx
@@ -1,7 +1,10 @@
+import ShareButton from "../components/ShareButton";
+
 const Membership = () => {
   return (
     <>
       <h1>會員中心</h1>
+      <ShareButton data={{ title: "Sharing" }} />
     </>
   );
 };
