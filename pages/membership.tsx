75eb5490ae025b55a84d4126e00772f1222adb7b
brianliu <hong86323@gmail.com>
2022-06-30 15:38:02 +0800

    Add ShareButton component

--- pages/membership.tsx
+++ pages/membership.tsx
@@ -1,21 +1,10 @@
-const Membership = () => {
-  const shareButtonClickHandler = async () => {
-    const data = {
-      title: "web.dev",
-      text: "Check out web.dev.",
-      url: "https://web.dev/",
-    };
-    try {
-      await navigator.share(data);
-    } catch (err) {
-      alert(err);
-    }
-  };
+import ShareButton from "../components/ShareButton";
 
+const Membership = () => {
   return (
     <>
       <h1>會員中心</h1>
-      <button onClick={shareButtonClickHandler}>Share</button>
+      <ShareButton data={{ title: "Sharing" }} />
     </>
   );
 };
