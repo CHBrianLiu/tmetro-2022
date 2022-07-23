effb46dd0c90d49c03abb7659d5345c60aeab411
Chunhung (Brian) Liu <hong86323@gmail.com>
2022-07-23 17:12:44 +0800

    Add "New badge acquired" notification and trigger (#9)
    * Adjust the font style within the TabBar.
    
    * Add badge acquisition notification modal

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,15 +3,38 @@ import Logo from "../assets/banner.metro-logo.svg";
 import Menu from "../assets/banner.hamburger-menu.svg";
 import Notification from "../assets/banner.notification-bell.svg";
 import Image from "next/image";
+import { useState } from "react";
+import Slide from "./Notification/Slide";
 
 const Banner = () => {
+  const [isModalVisible, setModalVisibility] = useState(false);
+
+  const openModal = () => {
+    console.warn("open");
+    setModalVisibility(true);
+  };
+  const closeModal = () => {
+    setModalVisibility(false);
+  };
+
   return (
-    <div className={styles.Banner}>
-      <Image src={Logo} className={styles.logo} />
-      <div className={styles.RightSection}>
-        <Image src={Notification} className={styles.notification} />
-        <Image src={Menu} className={styles.menu} />
+    <div>
+      <div className={styles.Banner}>
+        <Image src={Logo} className={styles.logo} />
+        <div className={styles.RightSection}>
+          <Image
+            src={Notification}
+            className={styles.notification}
+            onClick={openModal}
+          />
+          <Image src={Menu} className={styles.menu} />
+        </div>
       </div>
+      {isModalVisible ? (
+        <div style={{ zIndex: 1 }}>
+          <Slide closeModal={closeModal} />
+        </div>
+      ) : null}
     </div>
   );
 };
