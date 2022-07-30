89c5a607ee488bfa693657ce0218da55fd3fa785
mike.huang <hut326521@gmail.com>
2022-07-30 11:39:25 +0800

    1. fix coding style warning in the last commit. 2. fill Coupons and Achievements in MembershipTab with Fake Pic

--- components/MembershipTabs/Achievements.tsx
+++ components/MembershipTabs/Achievements.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
+import styles from "./Achievements.module.css";
+import AchieveFacePic1 from "../../assets/Membership/Achievements/AchieveFakePic1.svg";
+import AchieveFacePic2 from "../../assets/Membership/Achievements/AchieveFakePic2.svg";
+import AchieveFacePic3 from "../../assets/Membership/Achievements/AchieveFakePic3.svg";
+import AchieveFacePic4 from "../../assets/Membership/Achievements/AchieveFakePic4.svg";
+
 export const Achievements = () => {
   return (
     <div>
-      <h1>Achievements</h1>
+      <h2>我的成就徽章</h2>
+      <h3>已獲得3項徽章</h3>
+      <div className={styles.PointRecords}>
+        <Image src={AchieveFacePic1} />
+        <Image src={AchieveFacePic2} />
+        <Image src={AchieveFacePic3} />
+      </div>
+      <div className={styles.PointRecordsViewer}>
+        <Image src={AchieveFacePic4} />
+      </div>
     </div>
   );
 };
