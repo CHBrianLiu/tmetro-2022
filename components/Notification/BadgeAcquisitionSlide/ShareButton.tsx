72e415e928e928a3133ba610fb4d4992b87669cd
brianliu <hong86323@gmail.com>
2022-08-14 12:51:23 +0800

    Add badge sharing mechanism.

--- components/Notification/BadgeAcquisitionSlide/ShareButton.tsx
+++ components/Notification/BadgeAcquisitionSlide/ShareButton.tsx
@@ -5,9 +5,44 @@ const ShareButtonFrame = styled.div`
   width: 100%;
 `;
 
+interface Data {
+  title: string;
+  text?: string;
+  url?: string;
+  files?: File[];
+}
+
+const shareButtonClickHandler = async () => {
+  const response = await fetch(
+    "/assets/notification/badge-acquisition-slide/badge-for-sharing.png"
+  );
+  const badgeImage = await response.blob();
+  const badgeImageMetadata = {
+    type: "image/png",
+  };
+  const badgeImageName = "badge-for-sharing.png";
+  const badgeImageFile = new File(
+    [badgeImage],
+    badgeImageName,
+    badgeImageMetadata
+  );
+
+  const sharingData: Data = {
+    title: "北捷通勤王",
+    files: [badgeImageFile],
+    text: "跟你分享我的新徽章！",
+  };
+
+  try {
+    await navigator.share(sharingData);
+  } catch (err) {
+    console.warn(err);
+  }
+};
+
 export default function ShareButton() {
   return (
-    <ShareButtonFrame>
+    <ShareButtonFrame onClick={shareButtonClickHandler}>
       <Image
         src={"//assets/notification/badge-acquisition-slide/badge-sharing.webp"}
         height={111}
