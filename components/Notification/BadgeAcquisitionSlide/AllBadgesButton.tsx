0308bfa2876edc9142a644248789bb4fae56b644
brianliu <hong86323@gmail.com>
2022-08-14 11:58:44 +0800

    Fix Link usage error

--- components/Notification/BadgeAcquisitionSlide/AllBadgesButton.tsx
+++ components/Notification/BadgeAcquisitionSlide/AllBadgesButton.tsx
@@ -2,24 +2,24 @@ import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function AllBadgesButton({
-  closeModal,
-}: {
-  closeModal: () => void;
-}) {
+export default function AllBadgesButton(props: { closeModal: () => void }) {
   const AllBadgesButtonFrame = styled.div`
     width: 100%;
   `;
   return (
-    <AllBadgesButtonFrame onClick={closeModal}>
-      <Link href={"//membership?tab=achievements"}>
-        <Image
-          src={"//assets/notification/badge-acquisition-slide/all-badges.webp"}
-          width={810}
-          height={108}
-          layout={"responsive"}
-          alt={"all badges"}
-        />
+    <AllBadgesButtonFrame onClick={props.closeModal}>
+      <Link href={"/membership?tab=achievements"}>
+        <a>
+          <Image
+            src={
+              "//assets/notification/badge-acquisition-slide/all-badges.webp"
+            }
+            width={810}
+            height={108}
+            layout={"responsive"}
+            alt={"all badges"}
+          />
+        </a>
       </Link>
     </AllBadgesButtonFrame>
   );
