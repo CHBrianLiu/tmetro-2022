e5aab019f9c614cd1082b37767b68ed7307cabf8
brianliu <hong86323@gmail.com>
2022-08-17 23:54:10 +0800

    Add coupon redeem trigger

--- pages/coupons/coldstone/redeeming.tsx
+++ pages/coupons/coldstone/redeeming.tsx
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import { useState } from "react";
 import LastPageFrame from "../../../components/LastPageFrame";
 import Image from "next/image";
+import CloseButton from "../../../components/Notification/BadgeAcquisitionSlide/CloseButton";
+import Link from "next/link";
+
+/* TODO: Separate modal component out*/
 
 const ContentContainer = styled.div`
   margin: 40px 18px;
@@ -40,14 +45,94 @@ const NoticeIconFrame = styled.div`
   height: 34px;
 `;
 
-/*
- * Todo: redeem complete modal
- * */
+const ModalBackground = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  left: 0;
+  top: 0;
+  z-index: 1;
+  background: rgba(15, 15, 15, 0.3);
+`;
+
+const ModalWindow = styled.div`
+  top: 19.7%;
+  height: calc(100vh - 2 * (19.7%));
+  left: 4.6%;
+  width: calc(100vw - 2 * (4.6%));
+  z-index: 2;
+  background: #ffffff;
+  position: absolute;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding-top: 50px;
+  padding-bottom: 50px;
+`;
+
+const ConfirmButton = styled.a`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 8px 16px;
+
+  width: 79%;
+
+  background: #3478ab;
+  background-blend-mode: normal, color-dodge;
+
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 21px;
+  color: #ffffff;
+
+  border-radius: 35px;
+  border: transparent;
+`;
+
+const RedeemSuccessTitleSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+`;
+
+const RedeemSuccessTitle = styled.div`
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 25px;
+  letter-spacing: 0.38px;
+`;
+
+const RedeemSuccessfulDate = styled.div`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+`;
+
+const IceCreamPicFrame = styled.div`
+  width: 61.5%;
+`;
+
+const RedeemSuccessSummaryFrame = styled.div`
+  width: 79%;
+`;
+
 const ColdstoneCouponIntroductionPage = () => {
+  const [isModalOnScreen, setModalOnScreen] = useState(false);
+
+  const openModal = () => setModalOnScreen(true);
+  const closeModal = () => setModalOnScreen(false);
+
   return (
     <LastPageFrame previousPageUrl={"/coupons/coldstone"} title={"優惠券"}>
       <ContentContainer>
-        <TicketInfoGroup>
+        <TicketInfoGroup onClick={openModal}>
           <Image
             src={"//assets/coupons/coldstone/redeem-preview.webp"}
             height={471}
@@ -83,6 +168,40 @@ const ColdstoneCouponIntroductionPage = () => {
           </NoticeTextParagraph>
         </NoticeInfoGroup>
       </ContentContainer>
+      {isModalOnScreen ? (
+        <ModalBackground>
+          <ModalWindow>
+            <CloseButton onClick={closeModal} />
+            <RedeemSuccessTitleSection>
+              <RedeemSuccessTitle>兌換完成！</RedeemSuccessTitle>
+              <RedeemSuccessfulDate>
+                兌換日期：2022-05-21 19:00
+              </RedeemSuccessfulDate>
+            </RedeemSuccessTitleSection>
+            <IceCreamPicFrame>
+              <Image
+                src={"//assets/coupons/coldstone/icecream.webp"}
+                width={469}
+                height={354}
+                layout={"responsive"}
+                alt={"ice cream"}
+              />
+            </IceCreamPicFrame>
+            <RedeemSuccessSummaryFrame>
+              <Image
+                src={"//assets/coupons/coldstone/redeem-success-summary.webp"}
+                width={840}
+                height={348}
+                layout={"responsive"}
+                alt={"summary"}
+              />
+            </RedeemSuccessSummaryFrame>
+            <Link href={"/membership?tab=coupons"} passHref>
+              <ConfirmButton onClick={closeModal}>確認</ConfirmButton>
+            </Link>
+          </ModalWindow>
+        </ModalBackground>
+      ) : null}
     </LastPageFrame>
   );
 };
